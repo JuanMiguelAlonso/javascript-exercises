21695757ebf23ae89c2efa0cbc8b206af30f99a7
JuanMiguelAlonso <juanmi-alonso@hotmail.es>
2024-05-27 16:24:41 +0200

    Ejercicio 05 completado

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function( numA, numB ) {
+    let lowerNumber;
+    let higherNumber;
+
+    if (numA < 0 || numB < 0)
+        return "ERROR";
+
+    if (typeof numA != "number" || typeof numB != "number")
+        return "ERROR";
+
+    if (numA < numB) {
+        lowerNumber = numA;
+        higherNumber = numB;
+    }
+    else {
+        lowerNumber = numB;
+        higherNumber = numA;
+    }
+
+    let result = higherNumber;
+
+    for (let i=lowerNumber;i<higherNumber;i++) {
+        result = result+i;
+    }
+
+    return result;
 
 };
 
