b831bab325c1efde182aaf788fce1a4ef2df3c0d
JuanMiguelAlonso <juanmi-alonso@hotmail.es>
2024-06-04 11:10:48 +0200

    Ejercicio 09 solucionado

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function ( word ) {
+  const validCharacters = '0123456789abcdefghijklmnopqrstuvwxyz';
 
+  // Convert to lowercase, split to array of individual characters, filter only valid characters, then rejoin as new string
+  const resultString = word.toLowerCase().split('')
+    .filter((character) => validCharacters.includes(character)).join('');
+
+  // Create a new reversed string for comparison
+  const reversedString = resultString.split('').reverse().join('');
+
+  // Return the outcome of the comparison which will either be true or false
+  return resultString === reversedString;
+    
 };
 
 // Do not edit below this line
